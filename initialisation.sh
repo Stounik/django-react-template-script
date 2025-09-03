38ca5776b265cd7dfa3104b68321056a7c57ba46
Nikos Stougiannos <45014927+Stounik@users.noreply.github.com>
2025-09-03 10:31:30 +0000

    Refactor initialisation script for improved NVM setup and backend installation process

--- initialisation.sh
+++ initialisation.sh
@@ -21,11 +21,13 @@ sudo apt-get install -y python3 python3-pip python3-venv
 # Install NVM
 echo "Installing NVM..."
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+export NVM_DIR="/usr/local/share/nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
 # Install Node.js
 echo "Installing Node.js..."
+echo $NVM_DIR
 nvm install --lts
 nvm use --lts
 
@@ -34,16 +36,13 @@ echo "Setting up backend..."
 check_directory "$BACKEND_DIR"
 cd "$BACKEND_DIR" || exit 1
 python3 -m venv .venv
-source .venv/bin/activate
-pip install --upgrade pip
-pip install -r requirements.txt
-django-admin startproject config .
+.venv/bin/pip install --upgrade pip && .venv/bin/pip install -r requirements.txt && .venv/bin/django-admin startproject config .
 
 # Setup frontend
 echo "Setting up frontend..."
 check_directory "$FRONTEND_DIR"
 cd "$FRONTEND_DIR" || exit 1
-npm create vite@latest . -- --template react
+npm create vite@latest ./ -- --template react --force
 npm install
 
 echo "Setup complete!"
